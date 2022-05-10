58af2a99ed1d0b9230a96d073038e71a316d0814
gyudong0920 <gyudong1996@gmail.com>
2022-05-10 11:15:01 +0900

    MY SQL DB 연동 연습

--- db_connect/src/ch04/Iemployees.java
+++ db_connect/src/ch04/Iemployees.java
@@ -3,11 +3,19 @@ package ch04;
 import java.util.ArrayList;
 
 public interface Iemployees {
-
+	// 현재 근무하고있는 사람들의 부서명까지 출력
 	ArrayList<EmployeesDto> workingDept(String to_date);
+	
+	// 연봉을 80000 이상 받는 사람의 이름과 최근 연봉을 출력
 	ArrayList<EmployeesDto> salary80000(int salary, String to_date);
+	
+	// 현재 각 부서의 매니저이름을 출력
 	ArrayList<EmployeesDto> deptManager(String to_date);
+	
+	// title 별 연봉 출력
 	ArrayList<EmployeesDto> titleSalary(String to_date);
+	
+	// 현재 매니저들의 성별을 출력
 	ArrayList<EmployeesDto> genderManager(String to_date);
 	
 	
--- db_connect/src/ch05/Iemployees2.java
+++ db_connect/src/ch05/Iemployees2.java
@@ -3,9 +3,18 @@ package ch05;
 import java.util.ArrayList;
 
 public interface Iemployees2 {
+	// 최고연봉을 받는 사람의 정보를 출력해주세요
 	EmployeesDto2 maxSalary();
+	
+	// 위의 문제를 인라인뷰로 만들기
 	EmployeesDto2 maxSalaryInline();
+	
+	// 각 사원의 정보에 초봉정보까지 포함
 	ArrayList<EmployeesDto2> firstSalary();
+	
+	// 직급이 senior engineer 인 직원의 정보만 출력하기
 	ArrayList<EmployeesDto2> seniorEngineer(String title);
+	
+	// 연봉이 80_000 이상인 사람의 정보만 출력
 	void salary80000();
 }
